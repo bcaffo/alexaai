86e4f9b234d469df0a25fc3be81d06937830e7e3
Brian Caffo <bcaffo@users.noreply.github.com>
2017-06-10 16:25:41 -0400

    added the file locations for the conf and service files

--- lectures/03_using_plumber/install.sh
+++ lectures/03_using_plumber/install.sh
@@ -48,14 +48,14 @@ ufw -f enable
 #
 mkdir /var/plumber/hello
 cp /usr/local/lib/R/site-library/plumber/examples/10-welcome/* /var/plumber/hello/
-wget servicefile
+wget https://raw.githubusercontent.com/bcaffo/alexaai/master/lectures/03_using_plumber/servicefile
 mv servicefile /etc/systemd/system/plumber-hello.service
 systemctl daemon-reload
 systemctl start plumber-hello && sleep 1
 systemctl restart plumber-hello && sleep 1
 systemctl enable plumber-hello
 systemctl status plumber-hello
-wget conf
+wget https://raw.githubusercontent.com/bcaffo/alexaai/master/lectures/03_using_plumber/conf
 mv conf /etc/nginx/sites-available/plumber-apis/hello.conf
 systemctl reload nginx
 

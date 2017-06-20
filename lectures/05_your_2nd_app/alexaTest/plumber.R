c4255e1f50d4c871dbbb7e99e9fdd8a4a5a909ab
Brian Caffo <bcaffo@gmail.com>
2017-06-20 02:03:02 +0000

    Finished lecture 5

--- lectures/05_your_2nd_app/alexaTest/plumber.R
+++ lectures/05_your_2nd_app/alexaTest/plumber.R
@@ -4,13 +4,13 @@ library(alexar)
 #* @serializer unboxedJSON
 function(req){
   dispatchAlexaRequest(req, intent=function(name, slots, ...){
-    if (name=="intent1"){
-      alexaResponse(output="This is how I respond to intent1!")
-    } else if (name=="intent2"){
-      alexaResponse(output="This is what I say to intent2!")
+    if (name=="askIntent"){
+      alexaResponse(output="Thank you for asking, but try with some manners!")
+    } else if (name=="pleaseIntent"){
+      alexaResponse(output="You said the magic word!")
     }
   }, launch=function(){
-    alexaResponse("You can ask for intent1 or intent2")
+    alexaResponse("You can ask to test the app, try using please")
   })
 }
 
